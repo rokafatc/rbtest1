13e7b9e7f571a90e33f151101eba7ca86ca7c635
atcadmin <atcadmin@WIN-CEF5MT28KQ0.localdomain>
2013-10-13 18:30:54 +0900

    force commit

--- RbTest1Prj1/src/kr/co/robin1/Test1.java
+++ RbTest1Prj1/src/kr/co/robin1/Test1.java
@@ -6,6 +6,12 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World 1");
+		
+		TestDataClass1 tdc1 = new TestDataClass1();
+		tdc1.setIdxNumber(22);
+		String rtnMsg = String.format("Return Result: %d", tdc1.getIdxNumber());
+		System.out.println(rtnMsg);
+		
 		try {
 			System.in.read();
 		} catch (IOException e) {

ab2915fa11e6a1a40e05a5f70d6df667a8703c40
atcadmin <atcadmin@WIN-CEF5MT28KQ0.localdomain>
2013-10-13 17:27:14 +0900

    First Commit(without add-action)

---
+++ RbTest1Prj1/src/kr/co/robin1/TestDataClass1.java
@@ -0,0 +1,17 @@
+package kr.co.robin1;
+
+public class TestDataClass1 {
+	private int idxNumber;
+	
+	public TestDataClass1() {
+		this.idxNumber = 0;
+	}
+	
+	public int getIdxNumber() {
+		return 1;
+	}
+	
+	public void setIdxNumber(int idx) {
+		this.idxNumber = idx;
+	}
+}
